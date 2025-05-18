194ef84c33b7555ecf424ed0a2a99bfa0cf78b81
psndao <papesemoundao2016@gmail.com>
2025-05-18 11:15:25 +0200

    mjr

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,3 +1,4 @@
+# importer les bibliothèques nécessaires
 import json
 import requests
 import boto3
@@ -5,8 +6,8 @@ from datetime import datetime
 import os
 
 # Récupérer la clé API météo depuis les variables d’environnement Lambda
-WEATHER_API_KEY = os.environ.get('d82ff05272fb47cdb86210347251605')  # doit être défini dans Lambda
-BUCKET_NAME = os.environ.get('weather-datalake-projects-papes')
+WEATHER_API_KEY = os.environ.get('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')  
+BUCKET_NAME = os.environ.get('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 PREFIX = 'raw/'
 CITIES = ['Paris', 'London', 'Dakar']
 
@@ -19,7 +20,7 @@ def lambda_handler(event, context):
         response = requests.get(url)
 
         if response.status_code != 200:
-            print(f"❌ Erreur API pour {city} : {response.status_code} - {response.text}")
+            print(f"Erreur API pour {city} : {response.status_code} - {response.text}")
             continue
 
         data = response.json()
@@ -33,7 +34,7 @@ def lambda_handler(event, context):
             ContentType='application/json'
         )
 
-        print(f"✅ {city} sauvegardée : s3://{BUCKET_NAME}/{key}")
+        print(f" {city} sauvegardée : s3://{BUCKET_NAME}/{key}")
 
     return {
         'statusCode': 200,
